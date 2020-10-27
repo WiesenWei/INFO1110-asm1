14960bac579fb20ddc83fcbbac7260a36782844c
Jiabo Li <jili5381@uni.sydney.edu.au>
2020-10-28 02:19:26 +1100

    1

--- dorrigo_contact.py
+++ dorrigo_contact.py
@@ -1,4 +1,4 @@
-]
+
 """
    Dorrigo Mobile Phone Contact Class.
   
@@ -26,8 +26,8 @@
    
    The messages are stored in the instance variable. Provided for you.
    Unfortunately there are only a maximum number of messages maximum to store and no more. 
-   When there are more than N messages, oldest messages from this array are discarded and 
-   only the most recent N messages remain. 
+   When there are more than 20 messages, oldest messages from this array are discarded and 
+   only the most recent 20 messages remain. 
    
    The functions for chat history are 
      add_chat_message()
@@ -62,7 +62,7 @@
   
 """
 class dorrigo_contact:
-	
+
 	def __init__(self, fname, lname, pnumber):
 		""" construct a new dorrigo_contact
 		    first name
@@ -71,40 +71,66 @@ class dorrigo_contact:
 		    Initialise other values to sensible default
 		"""
 		self.fname = fname
-		pass
-	
+		self.lname = lname
+		self.pnumber = pnumber
+		self.chat_history = []
+
 	def get_first_name(self): 
 		""" return the first name of the contact """
 		return self.fname
 
 	def get_last_name(self): 
 		""" return the last name of the contact """
-		pass
+		return self.lname
 
 	def get_phone_number(self): 
 		""" return the phone number of the contact as a string """
-		pass
+		return self.pnumber
 
 	def update_first_name(self, first_name):
 		""" if first_name is None the method will do nothing and return. 
 			The name will only update if the datatype is appropriate
 		"""
-		pass
+		if type(first_name) != str:
+			return
+		elif first_name == None:
+			return
+		else:
+			self.fname = first_name
+			return self.fname
 
 	def update_last_name(self, last_name): 
 		""" if last_name is None the method will do nothing and return
 			The name will only update if the datatype is appropriate
 		"""
-		pass
+		if type(last_name) != str:
+			return
+		elif last_name == None:
+			return
+		else:
+			self.lname = last_name
+			return self.lname
 	
 	def update_phone_number(self, number): 
-		""" only allows integer numbers (long type) between 6 and 14 digits
+		""" only allows integer numbers (int type) between 6 and 14 digits
 		   no spaces allowed, or prefixes of + symbols
 		   leading 0 digits are allowed
 		   return True if successfully updated
 		   if number is None, number is set to an empty string and the method returns False
 		"""
-		pass
+		i = 0
+		std = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+		if number == None or len(str(number)) < 6 or len(str(number)) > 14:
+			return False
+		else:
+			while i < len(str(number)):
+				if str(number)[i] in std:
+					pass
+				else:
+					return False
+				i += 1
+			self.pnumber = str(number)
+			return True	
 	
 	def add_chat_message(self, who_said_it, message): 
 		""" add a new message to the chat
@@ -115,20 +141,39 @@ class dorrigo_contact:
 		   Hint: keep track of the position of the oldest or newest message!
 		   always returns True
 		"""
-		pass
+		msg = who_said_it + ": " + message
+		if len(self.chat_history) < 20:
+			self.chat_history.append(msg)
+			return True
+		else:
+			i = 0
+			old_chat_history = []
+			while i < len(self.chat_history):
+				old_chat_history[i] = self.chat_history[i]
+				i += 1
+			self.chat_history = []
+			i = 1
+			while i <= 20:
+				self.chat_history.append(old_chat_history[i])
+				i += 1
+			return True
 
 	def clear_chat_history(self): 
 		""" after this, both last and oldest message should be referring to index 0
 		   all entries of chat history are set to None
 		   always returns True
 		"""
-		pass
+		self.chat_history = []
+		return True
 
 	def get_last_message(self): 
 		""" returns the last message 
 		   	if no messages, returns None
 		"""
-		pass
+		if len(self.chat_history) == 0:
+			return
+		else:
+			return self.chat_history[len(self.chat_history) - 1]
 	
 	def get_oldest_message(self): 
 		""" returns the oldest message in the chat history
@@ -137,14 +182,28 @@ class dorrigo_contact:
 		   2) more than MAXIMUM_CHAT_HISTORY, returns the oldest
 		   returns None if no messages exist
 		"""
-		pass
+		if len(self.chat_history) == 0:
+			return
+		else:
+			return self.chat_history[0]
 
 
 	def create_copy(self): 
 		""" creates a copy of this contact
 		   returns a new dorrigo_contact object with all data same as the current object
 		"""
-		pass
+		copy = dorrigo_contact("Eric", "Lee", 1234567)
+		copy.fname = self.fname
+		copy.lname = self.lname
+		copy.pnumber = self.pnumber
+		i = 0
+		try:
+			while i < len(self.chat_history):
+				copy.chat_history[i] = self.chat_history[i]
+				i += 1
+		except IndexError:
+			copy.chat_history = []
+		return copy
 	
 	def print_messages_oldest_to_newest(self): 
 		""" -- NOT TESTED --
--- dorrigo_mobile.py
+++ dorrigo_mobile.py
@@ -18,7 +18,7 @@
  *    First name
  *    Last name
  *    Phone number
- *  A list of 10 possible contacts.
+ *  A list of N possible contacts.
  *    Each contact stores first name, last name, phone number and chat history up to 20 messages
  *  
  * The phone has functionality
@@ -50,27 +50,37 @@ class dorrigo_mobile:
 		"""
 		Every phone manufactured has the following attributes
 		 
-		 the phone is off
-		 the phone has battery life 25
-		 the phone is not connected
-		 the phone has signal strength 0
+		 # the phone is off
+		 # the phone has battery life 25
+		 # the phone is not connected
+		 # the phone has signal strength 0
 		 Each of the contacts stored in the array contacts has a None value
 		 
-		 the owner first name "Dorrigo"
-		 the owner last name is "Incorporated"
-		 the owner phone number is "180076237867"
-		 the owner chat message should have only one message 
+		 # the owner first name "Dorrigo"
+		 # the owner last name is "Incorporated"
+		 # the owner phone number is "180076237867"
+		 # the owner chat message should have only one message 
 		         "Thank you for choosing Dorrigo products"	
 		"""
-		#contacts = 
-		pass
+		self.power = False
+		self.battery = 25
+		self.network = False
+		self.signal = 0
+		self.owner = dorrigo_contact("Dorrigo", "Incorporated", "180076237867")
+		self.owner.chat_history.append("Dorrigo: Thank you for choosing Dorrigo products")
+		self.contacts = [None] * max_contacts
 
 	def get_copy_of_owner_contact(self):
 		"""  returns a copy of the owner contact details
 		  	 return None if the phone is off
+			 datatype of returned object is dorrigo_contact or None
 		"""
-		pass
-
+		if self.power == False:
+			return None
+		else:
+			copy = dorrigo_contact("Dorrigo", "Incorporated", "180076237867")
+			copy.chat_history.append("Dorrigo: Thank you for choosing Dorrigo products")
+			return copy
 
 	def add_contact(self, contact): 
 		""" only works if phone is on
@@ -78,32 +88,78 @@ class dorrigo_mobile:
 		   The method will find an element that is None and set it to be the contact.
 		   returns True if successful
 		"""
-		pass
+		if self.power == False:
+			return False
+		else:
+			i = 0
+			while i < len(self.contacts):
+				if self.contacts[i] == contact:
+					return False
+				if self.contacts[i] == None:
+					self.contacts[i] = contact
+					return True
+				i += 1
+			return False
 
 	def remove_contact(self, contact): 
 		""" only works if phone is on
 		   find the dorrigo_contact object and set the array element to None
 	 	   return True on successful remove
 		"""
-		pass
+		if self.power == False:
+			return False
+		i = 0
+		if contact == None:
+			return False
+		else:
+			while i < len(self.contacts):
+				if contact == self.contacts[i]:
+					self.contacts[i] = None
+					return True
+				i += 1
+			return False
 
 	def get_number_of_contacts(self): 
 		""" only works if phone is on
 		    returns the number of contacts, or -1 if phone is off
 		"""
-		pass
+		if self.power == False:
+			return -1
+		else:
+			i = 0
+			n = 0
+			while i < len(self.contacts):
+				if self.contacts[i] != None:
+					n += 1
+				i += 1
+			return n
 
 	def search_contact(self, name): 
 		""" only works if phone is on
 		   returns a list of all contacts that match first name OR last name
 		   if phone is off, or no results, None is returned
 		"""
-		pass
+		if self.power == False:
+			return
+		else:
+			i = 0
+			ls = []
+			while i < len(self.contacts):
+				if self.contacts[i] != None:
+					if name == self.contacts[i].fname or name == self.contacts[i].lname:
+						ls.append(self.contacts[i])
+				i += 1
+			if ls == []:
+				return
+			return ls
 
 	def is_phone_on(self): 
 		""" returns True if phone is on
 		"""
-		pass
+		if self.power == True:
+			return True
+		else:
+			return False
 
 	def set_phone_on(self, on): 
 		"""
@@ -114,12 +170,27 @@ class dorrigo_mobile:
 			return False if do not have enough battery level to turn on
 			return True otherwise
 		"""
-		pass
+		if on == True:
+			if self.battery >= 6:
+				self.power = True
+				self.battery -= 5
+				self.network = False
+				return True
+			else:
+				self.network = False
+				return False
+		elif on == False:
+			self.power = False
+			self.network = False
+			return True
 	
 	def get_battery_life(self): 
 		""" Return the battery life level as an integer. if the phone is off, zero is returned.
 		"""
-		pass
+		if self.power == False:
+			return 0
+		else:
+			return self.battery
 	
 	def change_battery(self, new_battery_level): 
 		""" Change battery of phone.
@@ -127,20 +198,38 @@ class dorrigo_mobile:
 		   If new_battery_level is outside manufacturer specification of [0,100], then 
 		   no changes occur and returns False.
 		"""
-		pass
+		if 0 <= new_battery_level <= 100:
+			self.battery  = new_battery_level
+			self.power = False
+			self.battery_level = new_battery_level
+			return True
+		else:
+			return False
 	
 	def is_connected_network(self): 
 		""" only works if phone is on. 
 		    returns True if the phone is connected to the network
 		"""
-		pass
+		if self.power == False:
+			self.network = False
+			return False
+		elif self.network == True:
+			return True
+		else:
+			return False
 	
 	def disconnect_network(self): 
 		""" only works if phone is on. 
 		    when disconnecting, the signal strength becomes zero
 		    always returns True
 		"""
-		pass
+		if self.power == False:
+			self.network = False
+			return True
+		else:
+			self.network = False
+			self.signal = 0
+			return True
 	
 	def connect_network(self):
 		""" only works if phone is on. 
@@ -152,14 +241,33 @@ class dorrigo_mobile:
 		    3) it will cost 2 battery life to do so
 		   returns the network connected status
 		"""
-		pass
+		if self.power == False:
+			return False
+		else:
+			if self.network == True:
+				return self.network
+			else:
+				if self.signal == 0:
+					self.signal = 1
+					self.network = True
+					self.use_phone(2)
+					return self.network
+				else:
+					self.network = True
+					self.use_phone(2)
+					return self.network
 	
 	def get_signal_strength(self): 
 		""" only works if phone is on. 
 		   returns a value in range [1,5] if connected to network
 		   otherwise returns 0
 		"""
-		pass
+		if self.power == False:
+			return 0
+		elif self.network == False:
+			return 0
+		else:
+			return self.signal
 
 	def set_signal_strength(self, new_strength): 
 		""" only works if phone is on. 
@@ -167,16 +275,39 @@ class dorrigo_mobile:
 		   signal of 0 disconnects network
 		   signal [1,5] can connect to network if not already connected
 		   if the signal is set outside the range [0,5], nothing will occur and will return False
+		   if the phone is not connected to a network and n > 0, it will connect to a network 
+		   and reduce the battery life by 2
 		   returns True on success
 		"""
-		pass
+		if self.power == False:
+			return False
+		else:
+			if new_strength >= 1 and new_strength <= 5:
+				if self.network == False:
+					self.use_phone(4)
+				else: 
+					self.use_phone(2)
+				self.network = True
+				self.signal = new_strength
+				return True
+			elif new_strength == 0:
+				self.network = False
+				self.signal = 0
+				return True
+			else:
+				return False
 	
 	def charge_phone(self): 
 		""" each charge increases battery level by 10
 		   the phone has overcharge protection and cannot exceed 100
 		   returns True if the phone was charged by 10, otherwise False
 		"""
-		pass
+		if self.battery > 90:
+			self.battery = 100
+			return False
+		elif 0 <= self.battery <= 90:
+			self.battery += 10
+			return True
 	
 	def use_phone(self, k):
 		""" Use the phone which costs k units of battery life.
@@ -184,5 +315,20 @@ class dorrigo_mobile:
 		   becomes zero and the phone turns off.
 		   returns True on successful use (not partial)
 		"""
-		pass
-	
+		try:
+			units = k
+			if units < self.battery:
+				self.battery -= units
+				return True
+			elif units == self.battery:
+				self.battery = 0
+				self.power = False
+				self.network = False
+				return True
+			else:
+				self.battery = 0
+				self.power = False
+				self.network = False
+				return False
+		except TypeError:
+			pass
